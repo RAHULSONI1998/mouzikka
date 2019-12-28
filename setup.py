adc539404d710d2e01bb3553e079a150f05abf42
RAHULSONI1998 <59302983+RAHULSONI1998@users.noreply.github.com>
2019-12-28 14:13:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Mouzikka',
+    version='1.0',
+    packages=['Mouzikka'],
+    url='D:/Python/PycharmProjects/Mouzikka/View.py',
+    license='',
+    author='ASUS',
+    author_email='',
+    description='my music app'
+)
